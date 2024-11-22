0c47ac46ef25dd3d7bdbc3d600c32bbf28d39f7f
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-22 17:59:03 +0100

    Zap aapt_version_code_defaults
    It was caused by package-cache in case Build.VERSION.INCREMENTAL
    doesn't change across builds, which is always the case for local
    builds, exporting BUILD_NUMBER to something != eng.$USER will fix
    it the next build.
    
    Change-Id: Ic602197e6711ced511a0b102b1140c5db4ec8397

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -24,7 +24,6 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += lmodroidGlobalVars
 SOONG_CONFIG_lmodroidGlobalVars += \
-    aapt_version_code \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_override_format_from_reserved \
@@ -110,7 +109,6 @@ TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
 TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
 
 # Soong value variables
-SOONG_CONFIG_lmodroidGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_lmodroidGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_lmodroidGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_lmodroidGlobalVars_target_health_charging_control_charging_path := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH)
